0c97e8652f24371f70ca0a108341db2be3f12de7
Александр Коровкин <greenteamer@bk.ru>
2017-12-04 22:32:17 +0300

    prroduct model start

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from products.models import Product, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+	model = Category
+	prepopulated_fields = {'slug': ('name', ), }
+
+
+class ProductAdmin(admin.ModelAdmin):
+	model = Product
+	prepopulated_fields = {'slug': ('name', ), }
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ManyToManyField(Category, blank=True)
+    name = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, null=True, blank=True)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
